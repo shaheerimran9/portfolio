ee231d46712e6f9c13239afd895286f7d1411045
Shaheer Imran <shaheerimran@Shaheers-MacBook.local>
2020-10-04 12:37:18 -0400

    Fix fetch issue for github authorization.

--- src/components/Homepage/Projects.js
+++ src/components/Homepage/Projects.js
@@ -1,27 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ProjectCard from './ProjectCard';
 
 const Projects = () => {
     const [projects, setProjects] = useState([]);
 
-    const getProjects = () => {
-        fetch('https://api.github.com/users/shaheerimran9/repos')
+    const getProjects = async () => {
+        await fetch('https://api.github.com/users/shaheerimran9/repos', {
+            method: "GET",
+            headers: {
+                Authorization: `Token ${process.env.REACT_APP_API_KEY}`
+            }
+        })
             .then(res => res.json())
             .then(data => {
                 setProjects(data);
             })
     };
-    getProjects();
+
+    useEffect(() => {
+        getProjects();
+    }, []);
 
     return (
         <section className="landing-projects">
             <h2 className="landing-projects__heading">Recent Projects</h2>
             <div className="landing-projects__wrapper">
                 {projects.slice(0, 3).map(project => {
-                    return <ProjectCard title={project.name} description={project.description} link={project.homepage}/>
+                    return <ProjectCard key={project.id} title={project.name} description={project.description} link={project.homepage} />
                 })};
             </div>
-
         </section>
     )
 };
