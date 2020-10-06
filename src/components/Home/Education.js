c0d8746344d0a3c0a53db04cc2e7ba4b63d1a50f
Shaheer Imran <shaheerimran@ShaheersMacBook.T-mobile.com>
2020-10-05 20:46:01 -0400

    Fix out of order college logos.

--- src/components/Home/Education.js
+++ src/components/Home/Education.js
@@ -7,7 +7,7 @@ const Education = () => {
         <section className="education">
             <h2 className="education__heading">Education</h2>
             <div className="education__school">
-                <img src={reynoldsImg} className="school__img" alt="college-logo"/>
+                <img src={lambdaImg} className="school__img" alt="college-logo"/>
                 <div className="school__info">
                     <h3 className="school__info__name">Lambda School</h3>
                     <h3 className="school__info__degree">Full Stack Web Development and Computer Science Bootcamp</h3>
@@ -18,7 +18,7 @@ const Education = () => {
                 </div>
             </div>
             <div className="education__school">
-                <img src={lambdaImg} className="school__img" alt="college-logo"/>
+                <img src={reynoldsImg} className="school__img" alt="college-logo"/>
                 <div className="school__info">
                     <h3 className="school__info__name">J. Sargeant Reynolds Community College</h3>
                     <h3 className="school__info__degree">Associate of Science: Business Administration</h3>
