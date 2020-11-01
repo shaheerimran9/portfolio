586e3b2bb6a535e82bf0117351d8934fc842a576
Shaheer Imran <shaheerimran@ShaheersMacBook.T-mobile.com>
2020-11-01 17:51:23 -0500

    Update education.

--- src/components/Home/Education.js
+++ src/components/Home/Education.js
@@ -1,30 +1,30 @@
 import React from 'react';
 import reynoldsImg from '../../img/reynolds.jpg';
-import lambdaImg from '../../img/lambdaschool.png'
+import vcuImg from '../../img/vcu.jpg'
 
 const Education = () => {
     return (
         <section className="education">
             <h2 className="education__heading">Education</h2>
             <div className="education__school">
-                <img src={lambdaImg} className="school__img" alt="college-logo"/>
+                <img src={vcuImg} className="school__img" alt="college-logo" />
                 <div className="school__info">
-                    <h3 className="school__info__name">Lambda School</h3>
-                    <h3 className="school__info__degree">Full Stack Web Development and Computer Science Bootcamp</h3>
+                    <h3 className="school__info__name">Virginia Commonwealth University</h3>
+                    <h3 className="school__info__degree">Bachelor of Science: Information Systems</h3>
                     <p className="schoool__info__progress">Currently in-progress</p>
                     <p className="school__info__description">
-                        A remote twelve month immersive, hands-on Full Stack Web Development and Computer Science program. Classes have a rigorous structure which helps ensure that students graduate with all the skills they need to make an impact on day one.
+                        A curriculum that combines technical computing knowledge, skills and techniques with relevant business knowledge.
                     </p>
                 </div>
             </div>
             <div className="education__school">
-                <img src={reynoldsImg} className="school__img" alt="college-logo"/>
+                <img src={reynoldsImg} className="school__img" alt="college-logo" />
                 <div className="school__info">
                     <h3 className="school__info__name">J. Sargeant Reynolds Community College</h3>
                     <h3 className="school__info__degree">Associate of Science: Business Administration</h3>
-                    <p className="schoool__info__progress">Graduated</p>
+                    <p className="schoool__info__progress">Graduated - 3.18 GPA</p>
                     <p className="school__info__description">
-                        A two-year preparing graduates to begin or advance their careers in the business world with a comprehensive understanding of management principles, business-oriented technology and interpersonal skills.
+                        A two-year program preparing graduates to begin or advance their careers in the business world with a comprehensive understanding of management principles, business-oriented technology and interpersonal skills.
                     </p>
                     <ul className="school__info__accomplishments">
                         <li>Applied ethical reasoning to business situations</li>
